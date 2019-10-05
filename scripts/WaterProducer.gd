e14a21d9142c38c3c2056c8b723c1bc396e1f607
Ethan Ransom <derethanhausen@gmail.com>
2019-10-05 14:13:45 -0600

    [Water System] Producers deliver water to Consumers within range

--- scripts/WaterProducer.gd
+++ scripts/WaterProducer.gd
@@ -5,18 +5,26 @@ export var deliveryRate:float
 # what priority to return
 export var water_priority:float
 
+var waterees = []
+
 func _ready():
 	pass
 
 func _process(delta):
-	var amount = deliveryRate * delta 
+	var amount = deliveryRate * delta
 	
-	var consumers = get_tree().get_nodes_in_group("WaterConsumers")
-	for consumer in []:
-		#var dist = consumer.get_global_pos().distance_to(self.get_global_pos())
-		#if dist < 10:
-		#	node.deliverWater(amount)
-		pass
+	var consumers = get_waterees()
+	for consumer in consumers:
+		consumer.deliverWater(amount)
+
+func get_waterees():
+	var in_range = $WateringArea.get_overlapping_areas()
+	var waterees = []
+	for bodies in in_range:
+		var parent = bodies.get_node("..")
+		if parent.is_in_group("WaterConsumers"):
+			waterees.push_back(parent)
+	return waterees
 
 # TODO: make dynamic based on distance to dry plants
 func evaluate_priority():
