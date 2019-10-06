6387e46ac1c79229a9c671c3c79500c5d1eb1fd2
Ethan Ransom <derethanhausen@gmail.com>
2019-10-05 23:40:31 -0600

    Finish Draggables system first pass, water only when current behavior

--- scripts/Draggable.gd
+++ scripts/Draggable.gd
@@ -4,13 +4,26 @@ var dragAttach = null
 
 onready var actor : Actor  = $"../"
 
-func _on_ClickableArea_input_event(viewport, event, shape_idx):
-    if event is InputEventMouseButton:
-        if event.is_pressed():
-            dragAttach = actor.get_local_mouse_position()
-        elif not event.is_pressed():
-            dragAttach = null
+func _ready():
+  actor.register_behavior(self)
 
 func _process(delta):
-    if dragAttach != null:
-        actor.position = actor.get_global_mouse_position() - dragAttach
+  if dragAttach != null:
+    actor.position = actor.get_global_mouse_position() - dragAttach
+
+func _on_ClickableArea_input_event(viewport, event, shape_idx):
+  print("on click")
+  if event is InputEventMouseButton:
+    if event.is_pressed():
+      dragAttach = actor.get_local_mouse_position()
+    elif not event.is_pressed():
+      dragAttach = null
+
+func execute_behavior():
+  pass
+
+func evaluate_priority():
+  if dragAttach == null:
+    return 0.0
+  else:
+    return 2.0
