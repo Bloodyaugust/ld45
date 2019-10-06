f2c170eab0bd5c8684c76e1ce4c76fca7fc92029
Jon Bardin <diclophis@gmail.com>
2019-10-06 02:49:01 -0400

    lets get it on

--- scripts/LiveAndDie.gd
+++ scripts/LiveAndDie.gd
@@ -6,6 +6,8 @@ export var ageRate:float
 onready var actor = $"../"
 
 var health = 1.0
+var age = 0.0
+var maxAge = 1.0
 
 func _ready():
   pass
@@ -13,16 +15,20 @@ func _ready():
 func _process(delta):
   _growOlder(delta * ageRate)
 
-  if _isTooOld():
+  if _isTooOld() || _isTooSick():
     _curlUpAndDie()
 
 func _curlUpAndDie():
   actor.emit_signal("dies")
 
 func _growOlder(amount):
-  health = _setHealthClamped(health - amount)
+  age += amount
+  #health = _setHealthClamped(health - amount)
 
 func _isTooOld():
+  return age >= maxAge
+
+func _isTooSick():
   return health <= 0.0
 
 func _setHealthClamped(unclampedHealth):
---
+++ scripts/ProcreateOrDie.gd
@@ -0,0 +1,47 @@
+extends Node
+class_name ProcreateOrDie
+
+onready var actor : Actor = $"../"
+onready var tree := get_tree()
+onready var root := tree.get_root()
+onready var _klassAlienWaterer : PackedScene = load("res://actors/AlienWaterer.tscn")
+onready var _klassAlienFighter : PackedScene = load("res://actors/AlienFighter.tscn")
+
+var desireToProcreate = 0.0
+var sexinessRate = 0.05
+var humpDistance = 200.0
+var chaseSpeed = 200.0
+
+func _process(delta):
+  _increaseSexDrive(delta * sexinessRate)
+
+func _increaseSexDrive(amount):
+  desireToProcreate += amount
+  
+func evaluate_priority():
+  #print("should_fuck?", desireToProcreate)
+  return desireToProcreate
+
+func execute_behavior():
+  for actorToHump in tree.get_nodes_in_group("Aliens"):
+    if actor != actorToHump:
+      actor.move_speed = chaseSpeed
+      actor.move_toward_target(actorToHump.position)
+      if actor.position.distance_to(actorToHump.position) < humpDistance:
+          desireToProcreate = 0.0
+          actorToHump.find_node("ProcreateOrDie").desireToProcreate = 0.0
+          if rand_range(0.0, 1.0) > 0.5:
+            var newAlienWaterer := _klassAlienWaterer.instance()
+            newAlienWaterer.position = actor.position + Vector2(0.0, 250.0)
+            root.add_child(newAlienWaterer)
+          else:
+            var newAlienFighter := _klassAlienFighter.instance()
+            newAlienFighter.position = actor.position + Vector2(0.0, 250.0)
+            root.add_child(newAlienFighter)                 
+
+          break
+          
+      break
+    
+func _ready():
+  actor.register_behavior(self)
