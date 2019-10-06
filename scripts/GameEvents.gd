cbc9a9a28cb74284e20df4f923f45f088d5d3775
Greyson Richey <greysonrichey@gmail.com>
2019-10-06 00:11:02 -0600

    Implement spawning an enemy-filled meteor
    - Also added media assets

--- scripts/GameEvents.gd
+++ scripts/GameEvents.gd
@@ -6,12 +6,17 @@ onready var root := tree.get_root()
 onready var level_base := root.find_node("LevelBase", true, false)
 onready var level_rect : Rect2 = level_base.get_rect()
 
+onready var _enemy_meteor : PackedScene = preload("res://actors/EnemyMeteor.tscn")
 onready var _startup_meteor : PackedScene = preload("res://actors/StartupMeteor.tscn")
 
 var _events : Array = [
   {
     "type": "condition",
     "function": "_do_startup_meteor"
+  },
+  {
+    "type": "condition",
+    "function": "_do_enemy_meteor"
   }
 ]
 var _rng : RandomNumberGenerator = RandomNumberGenerator.new()
@@ -40,3 +45,15 @@ func _do_startup_meteor():
       var new_startup_meteor := _startup_meteor.instance()
       new_startup_meteor.position = Vector2(level_rect.position.x + (level_rect.size.x * _rng.randf()), level_rect.position.y + (level_rect.size.y * _rng.randf()))
       root.add_child(new_startup_meteor)
+
+var _enemy_meteor_interval : float = 20
+var _time_to_enemy_meteor : float = 20
+func _do_enemy_meteor():
+  if tree.get_nodes_in_group("Aliens").size() > 0:
+    _time_to_enemy_meteor -= get_process_delta_time()
+    if _time_to_enemy_meteor <= 0:
+      _time_to_enemy_meteor = _enemy_meteor_interval
+
+      var new_enemy_meteor := _enemy_meteor.instance()
+      new_enemy_meteor.position = Vector2(level_rect.position.x + (level_rect.size.x * _rng.randf()), level_rect.position.y + (level_rect.size.y * _rng.randf()))
+      root.add_child(new_enemy_meteor)
