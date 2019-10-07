80e2f4d5711a54b1fec7cf1dcd72f6a17f9b551e
Jon Bardin <diclophis@gmail.com>
2019-10-07 00:32:23 -0400

    limit random walk rect

--- scripts/classes/IdleBehavior.gd
+++ scripts/classes/IdleBehavior.gd
@@ -2,6 +2,9 @@ extends Node
 class_name IdleBehavior
 
 onready var actor : Actor = $"../"
+onready var root := get_tree().get_root()
+onready var level_base := root.find_node("LevelBase", true, false)
+onready var level_rect : Rect2 = level_base.get_rect()
 
 var idleSpeed = 100.0
 var randomTarget = Vector2(0, 0)
@@ -18,11 +21,9 @@ func execute_behavior():
   actor.move_toward_target(randomTarget)
 
 func _pickRandomTarget():
-  var min_x = -1200
-  var max_x = 1200
-  var min_y = -1200
-  var max_y = 1200
-  randomTarget = Vector2(rand_range(min_x, max_x), rand_range(min_y, max_y))
+  var rx = (level_rect.position.x + (level_rect.size.x * randf()))
+  var ry = (level_rect.position.y + (level_rect.size.y * randf()))
+  randomTarget = Vector2(rx, ry)
 
 func _ready():
   _pickRandomTarget()
