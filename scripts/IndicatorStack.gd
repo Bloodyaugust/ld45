e34cdf85f69286aee8a2024f1093e2c196ed6e89
Jon Bardin <diclophis@gmail.com>
2019-10-07 01:07:17 -0400

    more indicators

--- scripts/IndicatorStack.gd
+++ scripts/IndicatorStack.gd
@@ -2,11 +2,15 @@ extends Node2D
 
 onready var love := $"LoveIndicator"
 onready var fear := $"FearIndicator"
-  
+onready var hunt := $"HuntIndicator"
+onready var farm := $"FarmIndicator"
+
 func _resetAll():
   love.emitting = false
   fear.emitting = false
-
+  hunt.emitting = false
+  farm.emitting = false
+  
 func indicateNothing():
   _resetAll()
 
@@ -17,6 +21,10 @@ func indicateForBehaviour(name):
       love.emitting = true
     "Fear":
       fear.emitting = true
+    "Hunt":
+      hunt.emitting = true
+    "Farming":
+      farm.emitting = true
     _:
       # arnt we all afraid a little bit sometimes
       if rand_range(0.0, 1.0) > 0.999:
