747dff77759c1197b48aa8de2d022ae9a2233c51
Greyson Richey <greysonrichey@gmail.com>
2019-10-07 00:11:10 -0600

    Add splash screen, make it run scene

--- scripts/classes/SquishyAnimate.gd
+++ scripts/classes/SquishyAnimate.gd
@@ -4,7 +4,8 @@ class_name SquishyAnimate
 export var animate_speed_scalar : float
 
 onready var sprite := $"../"
+onready var sprite_scale : Vector2 = sprite.scale
 
 func _process(delta):
   var x : float = float(OS.get_ticks_msec()) / 1000
-  sprite.scale = Vector2(1, 1) + (Vector2(1, 1) * ((sin(x * animate_speed_scalar) + 1) / 2)) * 0.2
+  sprite.scale = sprite_scale + (Vector2(1, 1) * ((sin(x * animate_speed_scalar) + 1) / 2)) * 0.2
